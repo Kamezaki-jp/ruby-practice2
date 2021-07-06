ef6371ec8facd4571b7e2f168f2adbb25f6b1865
Kamezaki-jp <champagne.es335@gmail.com>
2021-07-06 22:25:35 +0900

    ruby-practice

--- ruby.rb
+++ ruby.rb
@@ -1,7 +1,28 @@
-puts "a?"
-a = gets.to_i
-puts "b?"
-b = gets.to_i
+# 文字列を変換
+str = "hello"
+puts str.upcase
+str2 = "HELLO"
+puts str.downcase
 
-c =  a * b
-puts "a * b = #{c}"
+# 文字列を配列にする
+str = "こんにちは,おはよう,ruby"
+p str.split(',')
+
+# 特定の文字列を表示
+str = "こおんはによちうわ"
+
+str.each_char.with_index{|c, index|
+  # odd? 奇数かどうか
+  # even? 偶数かどうか
+  if index.odd?
+    print c
+  end
+}
+
+print "\n"
+
+# rubyで文字列を挿入する
+str = "Ruby,Python,PHP"
+str.insert(4, ",Java")
+puts str
+
