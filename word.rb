f3c792fb0bc4792983de4858636e0fbf440674fc
Kamezaki-jp <champagne.es335@gmail.com>
2021-07-16 23:21:27 +0900

    fix

--- word.rb
+++ word.rb
@@ -1,11 +1,20 @@
 n = gets.to_i
 m = gets.chomp
 
+array = Array.new()
+
 n.times do |i|
-    word = gets.chomp
-    if word.include? m
-        puts word
-    else
-        puts "None"
+    array[i] = gets.chomp
+end
+
+count = 0
+array.each do |arr|
+    if arr.include?(m)
+        puts arr
+        count += 1
     end
+end
+
+if count == 0
+    puts "None"
 end
